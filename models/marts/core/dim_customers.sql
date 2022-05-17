ec57dd51b903c565fc4430173366126bc98dd417
Christian Rua <christian.rua@autodesk.com>
2022-05-17 08:07:27 -0500

    refactoring the models

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,48 +1,21 @@
 
 with customers as (
-
     select * from {{ ref('stg_customers')}}
-
 ),
-
 orders as (
-
-    select * from {{ ref('stg_orders') }}
-
+    select * from {{ ref('fct_orders')}}
 ),
-
-payments as (
-
-    select * from {{ ref('stg_payments') }}
-
-),
-
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
-
-    group by 1
-
-),
-
-payments_sum_amount as (
-
-    select id,sum(amount) as lifetime_value
-
-    from payments
     group by 1
 ),
-
-
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
@@ -50,13 +23,8 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        payments_sum_amount.lifetime_value as lifetime_value
-
+        customer_orders.lifetime_value
     from customers
-
     left join customer_orders using (customer_id)
-    join payments_sum_amount on payments_sum_amount.id = customers.customer_id
-
 )
-
 select * from final
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,13 +1,12 @@
 with payments as (
     
     select
-        id,
+        id as payment_id,
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-        amount,
-        created,
-        _batched_at
+        amount / 100 as amount,
+        created as created_at
 
     from raw.stripe.payment
 )
