04e83d1ec4dcf9c38096658ff5f905fc8302dede
Christian Rua <christian.rua@autodesk.com>
2022-05-18 08:13:17 -0500

    trying to fix

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,13 +1,18 @@
-with payments as (
-
-    select * from {{ ref('stg_payments')}}
-
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
 ),
 
-orders as (
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
 
-    select * from {{ ref('stg_orders') }}
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
 
+    from payments
+    group by 1
 ),
 
 final as (
@@ -15,11 +20,11 @@ final as (
     select
         orders.order_id,
         orders.customer_id,
-        payments.amount
-        
-    from orders
-    join payments on orders.order_id = payments.order_id
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
 
+    from orders
+    left join order_payments using (order_id)
 )
 
 select * from final
